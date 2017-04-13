30088a49cd2fc130bf441d5fe968243e50c481c1
Gabriel Pereira <gsilvap1994@gmail.com>
2017-04-13 04:48:03 -0300

    deploy

--- public/js/answers.js
+++ public/js/answers.js
@@ -1,7 +1,8 @@
-var user ;/*$('#name').val(); */ // "Gabriel";
-var token ;//{{ token }};
+var user; /*$('#name').val(); */ // "Gabriel";
+var token; //{{ token }};
+var session;
 
-
+// on ready
 $( function() {
   $.ajax({
         url: '/api/user',
@@ -9,7 +10,7 @@ $( function() {
     }).done(function(user) {
         console.log(user);
         user = user;
-        $('#name').val(user.first_name);
+        $('#name').html(user.first_name);
         $('#messages').append("<li class ='message-mel'>"+greeting(user.first_name)+"</li>");
     });
 
@@ -20,65 +21,95 @@ $( function() {
       console.log(tokens);
       token = tokens;
     });
+
+    session = 'main';
 });
 
+// chama o controller quando o usuário clica em enviar
 $('#submit').click(function() {
 //  var name = $('#name').val();
-  var msgUser = $('#message-user').val();
-  var answer = answerQuestion(msgUser, name);
-  $('#messages').append("<li class='message-user'>"+ msgUser +"</li>");
-  $('#messages').append("<li class='message-mel'>"+ answer +"</li>");
-  if(msgUser === "Legal, adoro comprar pela internet! Qual produto você precisa?" || "Qual produto você deseja então?") {
-    // aqui vem a chamada do produto e listagem dos 3 com menor preço
-    // pegaProdutosPrimeiraPergunta
-    // pegarProdutosSegundaPergunta
-  }
-  if(msgUser === "Quer calcular o frete?") {
-    // função que  calcula o frete (optativo) (Faço a menor ideia de como)
-  }
+  mainController();
+  $('#message-user').val("");
 });
 
+// chama o controller quando o usuário pressiona o enter
+$('#message-user').keypress(function(e) {
+    if(e.which == 13) {
+        mainController();
+        $('#message-user').val("");
+        return false;
+    }
+});
 
-$('#selected-product').click(function() {
+// controller principal do bot
+function mainController() {
 
-})
+  var msgUser = $('#message-user').val();
+  $('#messages').append("<li class='message-user'>"+ msgUser +"</li>");
 
+  switch(session){
+    case 'main':
+      answerQuestion(msgUser);
+      break;
+    case 'search':
+      searchProduct(msgUser);
+      break;
+    case 'ship':
+      shipOrEnd(msgUser);
+      break;
+  }
+}
 
-function answerQuestion(question, name) {
-  alert(question+" "+ name);
+// menu para responder pergunta
+function answerQuestion(question) {
+  //alert(question+" "+ name);
   if( /^oi|ola|olá|eae|e aí|e ae/i.test(question)) {
-    return "Oi "+ name + ", como posso ajudar?";
+    print("Oi " +  ", como posso ajudar?");
   }
   else if (/^compra|acha|consulta|preço/i.test(question)) {
-    return "Legal, adoro comprar pela internet! Qual produto você precisa?";
+    session = 'search';
+    print("Legal, adoro comprar pela internet! Qual produto você precisa?");
   }
   else if (/^não|nao/i.test(question)){
-    return "Qual produto você deseja então?";
-  }
-  else if(question === "image") {
-    return "Quer calcular o frete?";
+    print("Qual produto você deseja então?");
   }
   else {
-    return "Precisa ser mais claro, ainda estou em construção, me desculpe.";
+    print("Precisa ser mais claro, ainda estou em construção, me desculpe.");
   }
 }
 
-function AnswerQuestion(question, name) {
-
+// menu para buscar produto
+function searchProduct(question) {
+  $.ajax({
+    url: 'https://api.mercadolibre.com/sites/MLB/search?q='+question.replace(/ /g, '+')+'&limit=3&access_token='+tokens.access_token,
+    method: 'get'
+  }).done(function(product) {
+    product.results.forEach(function(result) {
+      var _product = '<a href="#" class="selected-product"><span class='+result.seller_address.zip_code+'>'+result.title+'</span></a>';
+      print(_product);
+    });
+  });
 }
 
-
-
-/*  $('body').append(answerQuestion($('#message').val()));
-});
-
-function answerQuestion(keyword) {
-  alert(keyword);
-  searchJSON(keyword);
+// menu para calcular o frete ou finalizar a compra
+function shipOrEnd(question){
+  $('.selected-product').click(function() {
+    var zip_code_product = $(this).children('span').attr('class').toString();
+    print("Deseja saber o frete?");
+    if(/^sim|ok|desejo|quero|bem/i.test()) {
+      .ajax({
+        url: 'https://api.mercadolibre.com/sites/MLa/shipping_options?zip_code_from='+zip_code_product+'&to='user.zip_code,
+        method:'get'
+      }).done(function(shipping){
+        shipping.options.forEach(function(option) {
+          print(option.name+" Custo: "+option.cost+" R$");
+        });
+      });
+    }
+  });
 }
 
-function searchJSON(keyword) {
-  var parsedObjetc = JSON.parse('keyAndAns.json');
-  alert(parsedObject);
+// imprime na tela
+function print(answer) {
+  $('#messages').append("<li class='message-mel'>"+ answer +"</li>");
 }
-*/
